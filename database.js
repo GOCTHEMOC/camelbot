177435fd9027455ac02bc7546db726cb280c67b6
GOCTHEMOC <155336713+GOCTHEMOC@users.noreply.github.com>
2026-05-25 18:15:38 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Database = require("better-sqlite3");
 const db = new Database("camelbot.db");
 
-// SAFE schema (fixes your previous "no such column: id")
+// users table
 db.prepare(`
 CREATE TABLE IF NOT EXISTS users (
   discord_id TEXT PRIMARY KEY,
