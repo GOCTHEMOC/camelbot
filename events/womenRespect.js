1f1ccc210d777eaf69d60570db6886bafbd191ba
GOCTHEMOC <155336713+GOCTHEMOC@users.noreply.github.com>
2026-06-02 17:22:48 -0700

    Update womenRespect.js

--- events/womenRespect.js
+++ events/womenRespect.js
@@ -26,7 +26,7 @@ client.on("messageCreate", async (message) => {
   for (const phrase of disrespectfulPhrases) {
     if (content.includes(phrase)) {
       try {
-        await message.reply("HEY, we respect women around here 👎 :freshman_fanatic: 👎 :freshman_fanatic: 👎 :freshman_fanatic: 👎 :freshman_fanatic:");
+        await message.reply("HEY, we respect women around here 👎 <:freshman_fanatic:1501449425447555262> 👎 <:freshman_fanatic:1501449425447555262> 👎 <:freshman_fanatic:1501449425447555262> 👎 <:freshman_fanatic:1501449425447555262>");
       } catch (err) {
         console.error("❌ Failed to send women respect message:", err);
       }
